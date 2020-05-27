d44366c331d649026a9b4e727a60c071d94aed3c
z003kyek-e01 <aleksey.balykov.ext@siemens.com>
2020-05-28 00:56:58 +0300

    add all numbers

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let x = 0;
 
 function onInputChange(value) {
-    x = value
+    x = value;
 }
 
 
@@ -26,6 +26,134 @@ function interpolate() {
         {
             kk: 40,
             d: 7.6,
+        },
+        {
+            kk: 50,
+            d: 11.1,
+        },
+        {
+            kk: 60,
+            d: 14,
+        },
+        {
+            kk: 70,
+            d: 16.5,
+        },
+        {
+            kk: 80,
+            d: 18.7,
+        },
+        {
+            kk: 90,
+            d: 20.1,
+        },
+        {
+            kk: 100,
+            d: 20.8,
+        },
+        {
+            kk: 110,
+            d: 19.4,
+        },
+        {
+            kk: 120,
+            d: 16.7,
+        },
+        {
+            kk: 130,
+            d: 13.4,
+        },
+        {
+            kk: 140,
+            d: 9.8,
+        },
+        {
+            kk: 150,
+            d: 6.9,
+        },
+        {
+            kk: 160,
+            d: 4.6,
+        },
+        {
+            kk: 170,
+            d: 2.5,
+        },
+        {
+            kk: 180,
+            d: 1.1,
+        },
+        {
+            kk: 190,
+            d: 0.9,
+        },
+        {
+            kk: 200,
+            d: -1.2,
+        },
+        {
+            kk: 210,
+            d: -3.3,
+        },
+        {
+            kk: 220,
+            d: -6.5,
+        },
+        {
+            kk: 230,
+            d: -10.3,
+        },
+        {
+            kk: 240,
+            d: -13.2,
+        },
+        {
+            kk: 250,
+            d: -15.7,
+        },
+        {
+            kk: 260,
+            d: -17.9,
+        },
+        {
+            kk: 270,
+            d: -19.2,
+        },
+        {
+            kk: 280,
+            d: -18.1,
+        },
+        {
+            kk: 290,
+            d: -16.2,
+        },
+        {
+            kk: 300,
+            d: -13.9,
+        },
+        {
+            kk: 310,
+            d: -10.6,
+        },
+        {
+            kk: 320,
+            d: -8.3,
+        },
+        {
+            kk: 330,
+            d: -6.2,
+        },
+        {
+            kk: 340,
+            d: -4.4,
+        },
+        {
+            kk: 350,
+            d: -2.9,
+        },
+        {
+            kk: 360,
+            d: -2.5,
         },
     ];
 
@@ -53,7 +181,7 @@ function interpolate() {
 
     const result = fx1 + (x - x1) * ((fx2 - fx1) / (x2 - x1));
 
-    document.getElementById("result").innerHTML = result;
+    document.getElementById('result').innerHTML = result;
 
     console.log(min.kk);
     console.log(max.kk);
